64db574f8b076aff7bb1a331261c5455313ebecc
divyanshukmwt0408 <officialdivyanshu18@gmail.com>
2025-06-30 17:25:02 +0530

    home

--- src/main.js
+++ src/main.js
@@ -1,15 +1,23 @@
 import gsap from "gsap";
 import "./style.css";
+// function loader() {
+//   const loaderimg = document.querySelectorAll(".loaderImg");
+//   const tl = gsap.timeline();
 
-const loaderimg = document.querySelectorAll(".loaderImg");
-const tl = gsap.timeline({repeat : -1});
+//   loaderimg.forEach((img) => {
+//     tl.to(img, {
+//       opacity: 1,
+//       duration: 0.1,
+//     }).to(img, {
+//       opacity: 0,
+//       duration: 0.1,
+//     });
+//   });
 
-loaderimg.forEach((img) => {
-  tl.to(img, {
-    opacity: 1,
-    duration: 0.1,
-  }).to(img, {
-    opacity: 0,
-    duration :0.1,
-  });
-});
+//   tl.to("#loader", {
+//     height: 0,
+//     duration: 0.8,
+//     ease: "power2.inOut"
+//   });
+// }
+// loader()
