49687652e77cf27c2d65c796be377d6939fc50ec
divyanshukmwt0408 <officialdivyanshu18@gmail.com>
2025-07-01 12:19:18 +0530

    page2

--- src/main.js
+++ src/main.js
@@ -1,23 +1,47 @@
 import gsap from "gsap";
 import "./style.css";
-// function loader() {
-//   const loaderimg = document.querySelectorAll(".loaderImg");
-//   const tl = gsap.timeline();
+import ScrollTrigger from "gsap/ScrollTrigger";
 
-//   loaderimg.forEach((img) => {
-//     tl.to(img, {
-//       opacity: 1,
-//       duration: 0.1,
-//     }).to(img, {
-//       opacity: 0,
-//       duration: 0.1,
-//     });
-//   });
+gsap.registerPlugin(ScrollTrigger);
 
-//   tl.to("#loader", {
-//     height: 0,
-//     duration: 0.8,
-//     ease: "power2.inOut"
-//   });
-// }
-// loader()
+function loader() {
+  const loaderimg = document.querySelectorAll(".loaderImg");
+  const tl = gsap.timeline();
+
+  loaderimg.forEach((img) => {
+    tl.to(img, {
+      opacity: 1,
+      duration: 0.1,
+    }).to(img, {
+      opacity: 0,
+      duration: 0.1,
+    });
+  });
+
+  tl.to("#loader", {
+    height: 0,
+    duration: 0.8,
+    ease: "power2.inOut",
+  })
+
+  .set("#loader", {
+    display: "none"
+  });
+  tl.from(".hello", {
+    height :0,
+    duration:0.4,
+    stagger : 0.03,
+ease: "sine.out",
+}, "-=0.5")
+}
+loader();
+gsap.to("#video", {
+  scale:1,
+  scrollTrigger: {
+    trigger: "#page2",
+    scroller:"body",
+    start: "top 80%",
+    end: "top 30%",
+    scrub: 1,
+  }
+});
